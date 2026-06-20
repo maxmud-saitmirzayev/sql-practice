178580f4f0614c4a26e8ff31d5c86ee52780d9d2
maxmud-saitmirzayev <max.saitmirzayev@gmail.com>
2026-06-20 19:47:32 +0500

    subqueries.sql
    Subqueries in WHERE, FROM, correlated subqueries, ALL, NOT EXISTS - SQLZoo

--- SQLZoo/subqueries.sql
+++ SQLZoo/subqueries.sql
@@ -24,22 +24,17 @@ WITH   pog as (SELECT population FROM world  WHERE name = 'Germany')
   SELECT name, CONCAT(ROUND(100*population/(SELECT population FROM pog), 0), '%') as Percentage FROM world 
     WHERE continent = 'Europe'
 
--- Uzum ТАШ-110
--- Заказы выше средней суммы
-SELECT customer, amount FROM orders 
-  WHERE amount > (SELECT AVG(amount) FROM orders)
+  -- Задача 6: Какие страны имеют ВВП больше, чем любая другая страна Европы? [Укажите только название .] (Для некоторых стран значения ВВП могут быть нулевыми)
+SELECT name FROM world 
+  WHERE gdp > (SELECT MAX(gdp) FROM world 
+    WHERE continent = 'Europe')
 
--- Заказы выше средней суммы по своему статусу (коррелированный подзапрос)
-SELECT customer, amount FROM orders o 
-  WHERE amount > (SELECT AVG(amount) FROM orders 
-    WHERE status = o.status)
-
--- Напиши запрос который выводит средние суммы заказов по каждому статусу, но только те статусы где средняя сумма выше 50000.
-SELECT status, avg_amount FROM 
-  (SELECT status,  AVG(amount) as avg_amount FROM orders GROUP BY status) as sub  
-    WHERE avg_amount > 50000
-
--- То же самое задача который сверху через HAVING 
-SELECT status, AVG(amount) AS avg_amount FROM orders 
-  GROUP BY status 
+-- Задача 7: Найдите самую большую страну (по площади) на каждом континенте, укажите континент , его название и площадь :
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
     HAVING AVG(amount) > 50000
+
+
